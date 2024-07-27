07132d247e77ca92f8410f8ad6bc74b7ec066bc3
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-07-27 18:32:35 +0300

    booking form implemented

--- hypoportskylines/src/components/Bookings/Bookings.jsx
+++ hypoportskylines/src/components/Bookings/Bookings.jsx
@@ -2,9 +2,56 @@ import "./bookings.css";
 
 export const Bookings = () => {
     return (
-        <div className="Bookings">
-            {/* 1. The add new flight form  */}
+        <div className="bookings">
+            <form className="flight-form">
+                <div className="flight-form-entries">
+                    <div className="flight-form-entry">
+                        <p>Guest</p>
+                        <input type="text" placeholder="John Smith" className="flight-guest" />
+                    </div>
+                    <div className="flight-form-entry">
+                        <p>Departure Airport</p>
+                        <div>
+                            <select name="from" id="from" className="flight-depature-airport">
+                                <option value="">Please Select</option>
+                                <option value="">Sofia Airport</option>
+                                <option value="">Amsterdam Airport Schiphol</option>
+                                <option value="">Tokyo Haneda Airport</option>
+                                <option value="">London Heathrow</option>
+                                <option value="">Guangzhou Baiyun Airport</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flight-form-entry">
+                        <p>Destination Airport</p>
+                        <div>
+                            <select name="from" id="from" className="flight-destination-airport">
+                                <option value="">Please Select</option>
+                                <option value="">Sofia Airport</option>
+                                <option value="">Amsterdam Airport Schiphol</option>
+                                <option value="">Tokyo Haneda Airport</option>
+                                <option value="">London Heathrow</option>
+                                <option value="">Guangzhou Baiyun Airport</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="flight-form-entry">
+                        <p>Departure Date</p>
+                        <input type="date" name="date" className="flight-departure-date" />
+                    </div>
+
+                    <div className="flight-form-entry">
+                        <p>Date of Return</p>
+                        <input type="date" name="date" className="flight-date-of-return" />
+                    </div>
+                </div>
+
+                <button className="book-flight-form-btn">Book</button>
+            </form>
+
             {/* 2. create the table with all bookings */}
+            <div className="bookings-table">Table</div>
         </div>
     )
 }
--- hypoportskylines/src/components/Header/Header.jsx
+++ hypoportskylines/src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import "./header.css";
 export const Header = () => {
     return (
         <div className="header">
-            <div className="header-logo-title">
-                <img src=".././images/logo-img.jpg" alt="header image" className="header-img" />
+            <div className="header-logo-title-section">
+                <img src=".././images/logo-img.jpg" alt="header image" className="header-logo" />
 
                 <h2 className="header-title">HypoportSkyLines</h2>
             </div>
