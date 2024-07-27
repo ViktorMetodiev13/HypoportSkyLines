be7390f4931c75449b5346c5055660f997662e0e
ViktorMetodiev13 <miroslavov_victor_@abv.bg>
2024-07-28 00:03:25 +0300

    create a booking functionality created

--- hypoportskylines/src/components/Bookings/Bookings.jsx
+++ hypoportskylines/src/components/Bookings/Bookings.jsx
@@ -2,53 +2,125 @@ import "./bookings.css";
 
 import { useEffect, useState } from "react";
 
-import { getAllAirports } from "../../services/bookingService";
+import { createBooking, getAllAirports } from "../../services/bookingService";
 
 export const Bookings = () => {
     const [airports, setAirports] = useState([]);
+    const [bookingValues, setBookingValues] = useState({
+        'guest': '',
+        'departureAirport': '',
+        'destinationAirport': '',
+        'departureDate': '',
+        'dateOfReturn': '',
+    });
 
     useEffect(() => {
         getAllAirports()
             .then(result => {
-                setAirports(result)
-            })
+                setAirports(result);
+            });
     }, []);
+
+    const changeHandler = (e) => {
+        setBookingValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    };
+
+    const onCreateBookingSubmit = (e) => {
+        e.preventDefault();
+
+        const [firstName, lastName] = bookingValues.guest.split(" ");
+
+        const departureAirport = airports.find(airport => airport.title === bookingValues.departureAirport);
+        const destinationAirport = airports.find(airport => airport.title === bookingValues.destinationAirport);
+
+        const bookingModel = {
+            firstName: firstName || "",
+            lastName: lastName || "",
+            departureAirportId: departureAirport ? departureAirport.id : null,
+            arrivalAirportId: destinationAirport ? destinationAirport.id : null,
+            departureDate: bookingValues.departureDate,
+            returnDate: bookingValues.dateOfReturn,
+        };
+
+        console.log(bookingModel);
+
+        createBooking(bookingModel);
+    };
 
     return (
         <div className="bookings">
-            <form className="flight-form">
+            <form className="flight-form" onSubmit={onCreateBookingSubmit}>
                 <div className="flight-form-entries">
                     <div className="flight-form-entry">
                         <p>Guest</p>
-                        <input type="text" placeholder="John Smith" className="flight-guest" />
+                        <input
+                            type="text"
+                            placeholder="John Smith"
+                            className="flight-guest"
+                            name="guest"
+                            value={bookingValues.guest}
+                            onChange={changeHandler}
+                        />
                     </div>
                     <div className="flight-form-entry">
                         <p>Departure Airport</p>
                         <div>
-                            <select name="from" id="from" className="flight-depature-airport">
+                            <select
+                                name="departureAirport"
+                                id="from"
+                                className="flight-depature-airport"
+                                value={bookingValues.departureAirport}
+                                onChange={changeHandler}
+                            >
                                 <option value="">Please Select an airport</option>
-                                {airports.map(airport => <option value="" key={airport.id}>{airport.title}</option>)}
+                                {airports.map(airport =>
+                                    <option key={airport.id} value={airport.title}>
+                                        {airport.title}
+                                    </option>
+                                )}
                             </select>
                         </div>
                     </div>
                     <div className="flight-form-entry">
                         <p>Destination Airport</p>
                         <div>
-                            <select name="from" id="from" className="flight-destination-airport">
+                            <select
+                                name="destinationAirport"
+                                id="to"
+                                className="flight-destination-airport"
+                                value={bookingValues.destinationAirport}
+                                onChange={changeHandler}
+                            >
                                 <option value="">Please Select an airport</option>
-                                {airports.map(airport => <option value="" key={airport.id}>{airport.title}</option>)}
+                                {airports.map(airport =>
+                                    <option key={airport.id} value={airport.title}>
+                                        {airport.title}
+                                    </option>
+                                )}
                             </select>
                         </div>
                     </div>
 
                     <div className="flight-form-entry">
                         <p>Departure Date</p>
-                        <input type="date" name="date" className="flight-departure-date" />
+                        <input
+                            type="date"
+                            name="departureDate"
+                            className="flight-departure-date"
+                            value={bookingValues.departureDate}
+                            onChange={changeHandler}
+                        />
                     </div>
 
                     <div className="flight-form-entry">
                         <p>Date of Return</p>
-                        <input type="date" name="date" className="flight-date-of-return" />
+                        <input
+                            type="date"
+                            name="dateOfReturn"
+                            className="flight-date-of-return"
+                            value={bookingValues.dateOfReturn}
+                            onChange={changeHandler}
+                        />
                     </div>
                 </div>
 
--- hypoportskylines/src/services/bookingService.js
+++ hypoportskylines/src/services/bookingService.js
@@ -1,8 +1,39 @@
 const BASE_URL = 'https://interview.fio.de/core-frontend/api';
+// const AUTH_TOKEN = 'dcSPTXR7IQYtn2oMCfIAxvwNpOzGyU';
 
 export const getAllAirports = async () => {
     const response = await fetch(`${BASE_URL}/airports`);
     const result = await response.json();
+
+    return result;
+};
+
+export const getAllBookings = async () => {
+    const response = await fetch();
+    const result = await response.json();
+
+    return result;
+}
+
+export const createBooking = async (bookingData) => {
+    const response = await fetch(`https://interview.fio.de/core-frontend/api/bookings/create?authToken=dcSPTXR7IQYtn2oMCfIAxvwNpOzGyU`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            // 'X-Authorization': AUTH_TOKEN,
+        },
+        body: JSON.stringify(bookingData),     
+    });
+
+    // if (response.status === 401) {
+    //     return {};
+    // };
+
+    const result = await response.json();
+
+    // if (!response.ok) {
+    //     throw result;
+    // };
 
     return result;
 };
